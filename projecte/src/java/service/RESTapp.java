ff72c64d19f2ab829d2ee2a927294cc006702334
Angel Gascon <angel.gascon@estudiants.urv.cat>
2023-01-05 17:34:24 +0100

    Final frontend, requires revision firts part and documentation

--- projecte/src/java/service/RESTapp.java
+++ projecte/src/java/service/RESTapp.java
@@ -1,8 +1,20 @@
 package service;
 
 import jakarta.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @jakarta.ws.rs.ApplicationPath("/rest/api/v1/")
 public class RESTapp extends Application {
-    
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<>();
+        classes.add(AbstractFacade.class);
+        classes.add(CoinFacadeREST.class);
+        classes.add(ClientFacadeREST.class);
+        classes.add(PurchaseFacadeREST.class);
+        classes.add(CredentialsFacadeREST.class);
+        //classes.add(RESTRequestFilter.class);
+        return classes;
+    }
 }
